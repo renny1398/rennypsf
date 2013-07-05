fa5b79ba470ebf806385808bebc7a7de38ffe8d3
renny1398 <renny1398@gmail.com>
2013-07-05 21:54:07 +0900

    added psx/common.h and psx/psx.cc

---
+++ psx/common.h
@@ -0,0 +1,133 @@
+#pragma once
+
+
+typedef char s8;
+typedef unsigned char u8;
+typedef short s16;
+typedef unsigned short u16;
+typedef int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+
+
+////////////////////////////////
+// BFLIP Macros
+////////////////////////////////
+
+template<typename T> inline T BFLIP(T x)
+{
+    return x;
+}
+
+#ifdef MSB_FIRST    // for big-endian architecture
+template<> inline u16 BFLIP(u16 x)
+{
+    return ((x >> 8) & 0xff) | ((x & 0xff) << 8);
+}
+template<> inline u32 BFLIP(u32 x)
+{
+    return (x >> 24) | ((x >> 8) & 0xff00) | ((x << 8) & 0xff0000) | (x << 24);
+}
+//#else   // for little-endian architecture
+#endif
+
+inline u16 BFLIP16(u16 x) {
+    return BFLIP(x);
+}
+inline u32 BFLIP32(u32 x) {
+    return BFLIP(x);
+}
+
+
+namespace PSX {
+  typedef u32 PSXAddr;
+  typedef u32 PSXOffset;
+
+  class Composite;
+  class Component;
+
+  namespace R3000A {
+    class Processor;
+    class Interpreter;
+    class Recompiler;
+    struct Registers;
+  }
+
+  class RootCounter;
+  class HardwareRegisters;
+  class DMA;
+  class BIOS;
+}
+
+
+namespace SPU {
+  class SPUBase;
+  class SPUCore;
+  class SPU;
+  class SPU2;
+}
+
+
+
+namespace PSX {
+
+  class Component {
+
+  public:
+    explicit Component(Composite* composite);
+
+    Composite& Psx() { return composite_; }
+
+    u8 ReadMemory8(PSXAddr addr);
+    u16 ReadMemory16(PSXAddr addr);
+    u32 ReadMemory32(PSXAddr addr);
+
+    void WriteMemory8(PSXAddr addr, u8 value);
+    void WriteMemory16(PSXAddr addr, u16 value);
+    void WriteMemory32(PSXAddr addr, u32 value);
+
+    virtual s8& S8M_ref(PSXAddr addr);
+    virtual u8& U8M_ref(PSXAddr addr);
+    virtual s16& S16M_ref(PSXAddr addr);
+    virtual u16& U16M_ref(PSXAddr addr);
+    virtual u32& U32M_ref(PSXAddr addr);
+
+    virtual void* M_ptr(PSXAddr addr);
+    virtual s8* S8M_ptr(PSXAddr addr);
+    virtual u8* U8M_ptr(PSXAddr addr);
+    virtual u16* U16M_ptr(PSXAddr addr);
+    virtual u32* U32M_ptr(PSXAddr addr);
+
+    virtual u8& U8H_ref(PSXAddr addr);
+    virtual u16& U16H_ref(PSXAddr addr);
+    virtual u32& U32H_ref(PSXAddr addr);
+
+    virtual u8* U8H_ptr(PSXAddr addr);
+    virtual u16* U16H_ptr(PSXAddr addr);
+    virtual u32* U32H_ptr(PSXAddr addr);
+
+    virtual u32& U32R_ref(PSXAddr addr);
+
+    virtual void* R_ptr(PSXAddr addr);
+
+    R3000A::Processor& R3000a();
+    R3000A::Interpreter& Interp();
+    RootCounter& RCnt();
+
+    R3000A::Registers& R3000ARegs();
+    HardwareRegisters& HwRegs();
+    DMA& Dma();
+    BIOS& Bios();
+
+
+    // TODO: replace SPU with SPUCore
+    SPU::SPU& Spu();
+    SPU::SPU2& Spu2();
+
+  private:
+    Composite& composite_;
+  };
+
+
+}   // namespace PSX
---
+++ psx/psx.cc
@@ -0,0 +1,319 @@
+#include "psx.h"
+
+
+namespace PSX {
+
+
+  Composite::Composite()
+    : r3000a_(this), interp_(this, &r3000a_), rcnt_(this), spu_(this),
+      r3000a_regs_(), mem_(this), hw_regs_(this), dma_(this),
+      bios_(this) {}
+
+
+  void Composite::Init(bool enable_spu2) {
+  }
+
+
+  void Composite::Reset() {
+    r3000a_.Init();
+    rcnt_.Init();
+    spu_.Open();
+  }
+
+
+  R3000A::InterpreterThread* Composite::Run() {
+    Reset();
+    bios_.Init();
+    return interp_.Execute();
+  }
+
+
+  void Composite::Terminate() {
+    interp_.Shutdown();
+    mem_.Reset();
+  }
+
+
+
+  R3000A::Processor& Composite::R3000a() {
+    return r3000a_;
+  }
+
+  R3000A::Interpreter& Composite::Interp() {
+    return interp_;
+  }
+
+  RootCounter& Composite::RCnt() {
+    return rcnt_;
+  }
+
+  R3000A::Registers& Composite::R3000ARegs() {
+    return r3000a_regs_;
+  }
+
+  HardwareRegisters& Composite::HwRegs() {
+    return hw_regs_;
+  }
+
+  DMA& Composite::Dma() {
+    return dma_;
+  }
+
+  BIOS& Composite::Bios() {
+    return bios_;
+  }
+
+  SPU::SPU& Composite::Spu() {
+    return spu_;
+  }
+
+
+  void Composite::InitMemory() {
+    mem_.Init();
+  }
+
+
+  u8 Composite::ReadMemory8(PSXAddr addr) {
+    return mem_.Read8(addr);
+  }
+
+  u16 Composite::ReadMemory16(PSXAddr addr) {
+    return mem_.Read16(addr);
+  }
+
+  u32 Composite::ReadMemory32(PSXAddr addr) {
+    return mem_.Read32(addr);
+  }
+
+
+  void Composite::WriteMemory8(PSXAddr addr, u8 value) {
+    mem_.Write8(addr, value);
+  }
+
+  void Composite::WriteMemory16(PSXAddr addr, u16 value) {
+    mem_.Write16(addr, value);
+  }
+
+  void Composite::WriteMemory32(PSXAddr addr, u32 value) {
+    mem_.Write32(addr, value);
+  }
+
+
+  void Composite::Memcpy(PSXAddr dest, const void *src, int length) {
+    mem_.Copy(dest, src, length);
+  }
+
+
+/*
+  void Composite::Memcpy(void *dest, PSXAddr src, int length) const {
+    mem_.Copy(dest, src, length);
+  }
+*/
+
+  void Composite::Memset(PSXAddr dest, int data, int length) {
+    ::memset(mem_.M_ptr(dest), data, length);
+  }
+
+
+
+  s8& Composite::S8M_ref(PSXAddr addr) {
+    return mem_.S8M_ref(addr);
+  }
+
+  u8& Composite::U8M_ref(PSXAddr addr) {
+    return mem_.U8M_ref(addr);
+  }
+
+  s16& Composite::S16M_ref(PSXAddr addr) {
+    return mem_.S16M_ref(addr);
+  }
+
+  u16& Composite::U16M_ref(PSXAddr addr) {
+    return mem_.U16M_ref(addr);
+  }
+
+  u32& Composite::U32M_ref(PSXAddr addr) {
+    return mem_.U32M_ref(addr);
+  }
+
+
+  void* Composite::M_ptr(PSXAddr addr) {
+    return mem_.M_ptr(addr);
+  }
+
+  s8* Composite::S8M_ptr(PSXAddr addr) {
+    return mem_.S8M_ptr(addr);
+  }
+
+  u8* Composite::U8M_ptr(PSXAddr addr) {
+    return mem_.U8M_ptr(addr);
+  }
+
+  u16* Composite::U16M_ptr(PSXAddr addr) {
+    return mem_.U16M_ptr(addr);
+  }
+
+  u32* Composite::U32M_ptr(PSXAddr addr) {
+    return mem_.U32M_ptr(addr);
+  }
+
+
+  u8& Composite::U8H_ref(PSXAddr addr) {
+    return mem_.U8H_ref(addr);
+  }
+
+  u16& Composite::U16H_ref(PSXAddr addr) {
+    return mem_.U16H_ref(addr);
+  }
+
+  u32& Composite::U32H_ref(PSXAddr addr) {
+    return mem_.U32H_ref(addr);
+  }
+
+
+  u8* Composite::U8H_ptr(PSXAddr addr) {
+    return mem_.U8H_ptr(addr);
+  }
+
+  u16* Composite::U16H_ptr(PSXAddr addr) {
+    return mem_.U16H_ptr(addr);
+  }
+
+  u32* Composite::U32H_ptr(PSXAddr addr) {
+    return mem_.U32H_ptr(addr);
+  }
+
+
+  u32& Composite::U32R_ref(PSXAddr addr) {
+    return mem_.U32R_ref(addr);
+  }
+
+
+  void* Composite::R_ptr(PSXAddr addr) {
+    return mem_.R_ptr(addr);
+  }
+
+
+
+// Component
+
+  Component::Component(Composite *composite)
+    : composite_(*composite) {}
+
+  R3000A::Processor& Component::R3000a() { return composite_.R3000a(); }
+  R3000A::Interpreter& Component::Interp() { return composite_.Interp(); }
+  RootCounter& Component::RCnt() { return composite_.RCnt(); }
+
+  R3000A::Registers& Component::R3000ARegs() { return composite_.R3000ARegs(); }
+  HardwareRegisters& Component::HwRegs() { return composite_.HwRegs(); }
+  DMA& Component::Dma() { return composite_.Dma(); }
+  BIOS& Component::Bios() { return composite_.Bios(); }
+
+
+  SPU::SPU& Component::Spu() { return composite_.Spu(); }
+
+
+  u8 Component::ReadMemory8(PSXAddr addr) {
+    return composite_.ReadMemory8(addr);
+  }
+
+  u16 Component::ReadMemory16(PSXAddr addr) {
+    return composite_.ReadMemory16(addr);
+  }
+
+  u32 Component::ReadMemory32(PSXAddr addr) {
+    return composite_.ReadMemory32(addr);
+  }
+
+
+  void Component::WriteMemory8(PSXAddr addr, u8 value) {
+    composite_.WriteMemory8(addr, value);
+  }
+
+  void Component::WriteMemory16(PSXAddr addr, u16 value) {
+    composite_.WriteMemory16(addr, value);
+  }
+
+  void Component::WriteMemory32(PSXAddr addr, u32 value) {
+    composite_.WriteMemory32(addr, value);
+  }
+
+
+  s8& Component::S8M_ref(PSXAddr addr) {
+    return composite_.S8M_ref(addr);
+  }
+
+  u8& Component::U8M_ref(PSXAddr addr) {
+    return composite_.U8M_ref(addr);
+  }
+
+  s16& Component::S16M_ref(PSXAddr addr) {
+    return composite_.S16M_ref(addr);
+  }
+
+  u16& Component::U16M_ref(PSXAddr addr) {
+    return composite_.U16M_ref(addr);
+  }
+
+  u32& Component::U32M_ref(PSXAddr addr) {
+    return composite_.U32M_ref(addr);
+  }
+
+
+  void* Component::M_ptr(PSXAddr addr) {
+    return composite_.M_ptr(addr);
+  }
+
+  s8* Component::S8M_ptr(PSXAddr addr) {
+    return composite_.S8M_ptr(addr);
+  }
+
+  u8* Component::U8M_ptr(PSXAddr addr) {
+    return composite_.U8M_ptr(addr);
+  }
+
+  u16* Component::U16M_ptr(PSXAddr addr) {
+    return composite_.U16M_ptr(addr);
+  }
+
+  u32* Component::U32M_ptr(PSXAddr addr) {
+    return composite_.U32M_ptr(addr);
+  }
+
+
+  u8& Component::U8H_ref(PSXAddr addr) {
+    return composite_.U8H_ref(addr);
+  }
+
+  u16& Component::U16H_ref(PSXAddr addr) {
+    return composite_.U16H_ref(addr);
+  }
+
+  u32& Component::U32H_ref(PSXAddr addr) {
+    return composite_.U32H_ref(addr);
+  }
+
+
+  u8* Component::U8H_ptr(PSXAddr addr) {
+    return composite_.U8H_ptr(addr);
+  }
+
+  u16* Component::U16H_ptr(PSXAddr addr) {
+    return composite_.U16H_ptr(addr);
+  }
+
+  u32* Component::U32H_ptr(PSXAddr addr) {
+    return composite_.U32H_ptr(addr);
+  }
+
+
+  u32& Component::U32R_ref(PSXAddr addr) {
+    return composite_.U32R_ref(addr);
+  }
+
+
+  void* Component::R_ptr(PSXAddr addr) {
+    return composite_.R_ptr(addr);
+  }
+
+}   // namespace PSX
